209282e60a9ac3798a94be6c4473def648930c41
rajeshree <rajeshreesingh66580@gmail.com>
2020-12-23 20:24:12 +0530

    made cart page,auth folder

--- server.js
+++ server.js
@@ -8,14 +8,35 @@ const PORT=process.env.port||7000
 app.use(express.static('public'))
 
 
-app.get('/',function(req,res){
-    //res.send('Hello from server')
-  res.render('home')  
-})
 //set template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+//so that your layout works keep routes sfter setting engine
+
+app.get('/',function(req,res){
+  //res.send('Hello from server')
+res.render('home')  
+})
+
+app.get('/cart',function(req,res){
+//res.send('Hello from server')
+res.render('customers/cart')  ;
+})
+app.get('/login',function(req,res){
+  //res.send('Hello from server')
+  res.render('auth/login')  ;
+  })
+  app.get('/register',function(req,res){
+    //res.send('Hello from server')
+    res.render('auth/register')  ;
+    })
+    
+
+
+
+
 app.listen(PORT,()=> {
 
 console.log(`listeing on port ${PORT}`)
